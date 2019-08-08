004a68837d3418725908e2f176035e0b6257be77
Roger <12768403+rogix@users.noreply.github.com>
2019-08-08 14:04:14 -0300

    chores: add main page styles

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,81 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import './Main.css';
+
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
-	return <h1>{match.params.id}</h1>;
+	return (
+		<div className="main-container">
+			<img src={logo} alt="Tindev" />
+			<ul>
+				<li>
+					<img src="https://avatars3.githubusercontent.com/u/12768403?v=4" alt="" />
+					<footer>
+						<strong>Rogerio</strong>
+						<p>Programador front end</p>
+					</footer>
+
+					<div className="buttons">
+						<button type="button">
+							<img src={like} alt="Like" />
+						</button>
+						<button type="button">
+							<img src={dislike} alt="Dislike" />
+						</button>
+					</div>
+				</li>
+				<li>
+					<img src="https://avatars3.githubusercontent.com/u/12768403?v=4" alt="" />
+					<footer>
+						<strong>Rogerio</strong>
+						<p>Programador front end</p>
+					</footer>
+
+					<div className="buttons">
+						<button type="button">
+							<img src={like} alt="Like" />
+						</button>
+						<button type="button">
+							<img src={dislike} alt="Dislike" />
+						</button>
+					</div>
+				</li>
+				<li>
+					<img src="https://avatars3.githubusercontent.com/u/12768403?v=4" alt="" />
+					<footer>
+						<strong>Rogerio</strong>
+						<p>Programador front end</p>
+					</footer>
+
+					<div className="buttons">
+						<button type="button">
+							<img src={like} alt="Like" />
+						</button>
+						<button type="button">
+							<img src={dislike} alt="Dislike" />
+						</button>
+					</div>
+				</li>
+				<li>
+					<img src="https://avatars3.githubusercontent.com/u/12768403?v=4" alt="" />
+					<footer>
+						<strong>Rogerio</strong>
+						<p>Programador front end</p>
+					</footer>
+
+					<div className="buttons">
+						<button type="button">
+							<img src={like} alt="Like" />
+						</button>
+						<button type="button">
+							<img src={dislike} alt="Dislike" />
+						</button>
+					</div>
+				</li>
+			</ul>
+		</div>
+	);
 }
