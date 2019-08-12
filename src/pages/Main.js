02036440df6e54120bdf5ded4047d5f5f085993b
Roger <12768403+rogix@users.noreply.github.com>
2019-08-11 23:22:31 -0300

    chores: add mactching messages

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 
 import './Main.css';
@@ -8,9 +9,11 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
 	const [users, setUsers] = useState([]);
+	const [matchDev, setMatchDev] = useState(null);
 
 	useEffect(
 		() => {
@@ -24,6 +27,19 @@ export default function Main({ match }) {
 				setUsers(response.data);
 			}
 			loadUsers();
+		},
+		[match.params.id]
+	);
+
+	useEffect(
+		() => {
+			const socket = io('http://localhost:3333', {
+				query: { user: match.params.id }
+			});
+
+			socket.on('match', dev => {
+				setMatchDev(dev);
+			});
 		},
 		[match.params.id]
 	);
@@ -59,12 +75,12 @@ export default function Main({ match }) {
 								<p>{user.bio}</p>
 							</footer>
 
-							<div className="buttons" onClick={() => handleLike(user._id)}>
-								<button type="button">
-									<img src={like} alt="Like" />
-								</button>
+							<div className="buttons">
 								<button type="button" onClick={() => handleDislike(user._id)}>
 									<img src={dislike} alt="Dislike" />
+								</button>
+								<button type="button" onClick={() => handleLike(user._id)}>
+									<img src={like} alt="Like" />
 								</button>
 							</div>
 						</li>
@@ -72,6 +88,20 @@ export default function Main({ match }) {
 				</ul>
 			) : (
 				<div className="empty">Acabou :(</div>
+			)}
+
+			{matchDev && (
+				<div className="match-container">
+					<img src={itsamatch} alt="It's a match" />
+					<img className="avatar" src={matchDev.avatar} alt="" />
+					<strong>{matchDev.name}</strong>
+
+					<p>{matchDev.bio}</p>
+
+					<button type="button" onClick={() => setMatchDev(null)}>
+						Fechar
+					</button>
+				</div>
 			)}
 		</div>
 	);
